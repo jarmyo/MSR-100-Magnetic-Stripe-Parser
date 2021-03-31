4157426168229e10540753980b5b19d344664027
Julian Augusto Ruiz Martinez <jarm.yo@gmail.com>
2021-03-31 14:51:03 -0600

    Update Tests.cs

--- MSR-100-Magnetic-Stripe-ParserTests/Tests.cs
+++ MSR-100-Magnetic-Stripe-ParserTests/Tests.cs
@@ -16,6 +16,20 @@ namespace MSR_100_Magnetic_Stripe_ParserTests
             var result = Repos.MSR100Controller.MSR100Controller.ParseData(data);
             Assert.AreEqual(result.Track1.PAN, "5168755544412233");
         }
+        [Test]
+        public void CheckPAN2()
+        {
+            var data = "%B5350290149345177^FATEHI/SUALEH^16042010000000000000000000000000000567001000?;5350290149345177=16042010000056700100?";
+            var result = Repos.MSR100Controller.MSR100Controller.ParseData(data);
+            Assert.AreEqual(result.Track1.PAN, "5350290149345177");
+        }
+        [Test]
+        public void CheckNAME2()
+        {
+            var data = "%B5350290149345177^FATEHI/SUALEH^16042010000000000000000000000000000567001000?;5350290149345177=16042010000056700100?";
+            var result = Repos.MSR100Controller.MSR100Controller.ParseData(data);
+            Assert.AreEqual(result.Track1.Name, "FATEHI/SUALEH");
+        }
 
         [Test]
         public void CheckLongTrack1()
